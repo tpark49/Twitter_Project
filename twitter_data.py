79399a5d8c2bb2e6a8d029b9431412ac45181670
tpark49 <typ5120@gmail.com>
2021-11-07 01:38:14 -0400

    Update twitter_data.py

--- twitter_data.py
+++ twitter_data.py
@@ -1,12 +1,6 @@
 import tweepy
 import pandas as pd
 
-ACCESS_TOKEN = "2268107551-SAp0p06thgCMcqJbUylWuKW1HraVEoERdszdByO"
-ACCESS_SECRET = "ZKi6C3kOFtub428vOqYAjE33y0HRGhbuNImJLP17iMZPX"
-CONSUMER_KEY = "ROxcJwvOHLYnHmyxiJmC3gpEc"
-CONSUMER_SECRET = "JmloZ5TK6JNZho3JjIsqJLg5vJaebZOsisCUAMba3nmgqOARQc"
-
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit = True)
